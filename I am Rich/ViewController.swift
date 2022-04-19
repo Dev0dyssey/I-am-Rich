6a40111ab472236fa363cb3132701593c2136025
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-04-19 08:56:29 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Thomas Martin on 19/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
